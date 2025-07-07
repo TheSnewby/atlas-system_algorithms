1a1a4f628ec567d98a3ebc4b1f03a8f0a8bd4eb5
TheSnewby <stephen.j.newby@gmail.com>
2025-07-07 14:57:52 -0500

    added library and documentation for struct

--- red_black_tree/rb_trees.h
+++ red_black_tree/rb_trees.h
@@ -1,6 +1,7 @@
 #ifndef rb_trees
 #define rb_trees
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef enum
 {
@@ -8,6 +9,14 @@ typedef enum
 	BLACK = 1
 } rb_color_t;
 
+/**
+ * struct rb_tree - red-black tree node
+ * @value: value of current node
+ * @parent: parent node of current node
+ * @left: left node of current node
+ * @right: right node of current node
+ * @color: red-black color of current node
+ */
 typedef struct rb_tree
 {
 	int value;
