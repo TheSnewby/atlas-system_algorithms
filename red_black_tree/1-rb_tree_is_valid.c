eb26fc11ee8b74d8dd916e3364b4d21bfd448217
TheSnewby <stephen.j.newby@gmail.com>
2025-07-09 00:08:39 -0500

    added a black height check

--- red_black_tree/1-rb_tree_is_valid.c
+++ red_black_tree/1-rb_tree_is_valid.c
@@ -1,6 +1,35 @@
 #include "rb_trees.h"
 
 /**
+ * bheight_check - ensures black height is the same for all null leaves
+ *  descends tree to the first leaf, sets first value to 1, then traverses
+ * @tree: root node
+ *
+ * Return: positive if it's a valid for a red-black tree, -1 otherwise
+ */
+int bheight_check(const rb_tree_t *tree)
+{
+	int bh_left, bh_right;
+
+	if (!tree)
+		return (1);
+
+	bh_left = bheight(tree->left);
+	bh_right = bheight(tree->right);
+
+	if ((bh_left == -1) && (bh_right == -1))
+		return (-1);
+	else if (bh_left != bh_right)
+		return (-1);
+
+	if (tree->color == BLACK)
+		return (1 + bh_left);
+	else
+		return (bh_left);
+}
+
+
+/**
  * rb_tree_valid_recurse - recursive check for whether a tree is a valid RBT
  * @tree: root node to tree
  * @min: minimum allowed R Value
@@ -21,7 +50,7 @@ int rb_tree_valid_recurse(const rb_tree_t *tree, int min, int max)
 	{
 		if ((tree->color == RED) && (tree->left->color == RED))
 			return (0);
-		if ((tree->left->n < tree -> n) && (tree->left->n > min))
+		if ((tree->left->n < tree->n) && (tree->left->n > min))
 		{
 			left_return = rb_tree_valid_recurse(tree->left, min, tree->n);
 		}
@@ -57,6 +86,9 @@ int rb_tree_is_valid(const rb_tree_t *tree)
 
 	min = INT_MIN;
 	max = INT_MAX;
+
+	if (bheight_check(tree) == -1)
+		return (0);
 
 	return (rb_tree_valid_recurse(tree, min, max));
 }
