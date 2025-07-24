931bf1707fc12e85cef28d29e691efef0154bd76
TheSnewby <stephen.j.newby@gmail.com>
2025-07-24 18:44:46 -0500

    heap_create

--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -1,1 +1,25 @@
-heap_t *heap_create(int (*data_cmp)(void *, void *));
+#include "heap.h"
+
+/**
+ * heap_create - creates a new heap
+ * @data_cmp: the function to compare two nodes
+ *
+ * Return: pointer to new heap, or NULL on failure
+ */
+heap_t *heap_create(int (*data_cmp)(void *, void *))
+{
+	heap_t *heap = NULL;
+
+	if (!data_cmp)
+		return (NULL);
+
+	heap = (heap_t *)malloc(sizeof(heap_t));
+	if (!heap)
+		return (NULL);
+
+	heap->data_cmp = data_cmp;
+	heap->root = NULL;
+	heap->size = 0;
+
+	return (heap);
+}
