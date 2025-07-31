9389bb2b866d8e3eaa94865c4124ebd0c92d3411
TheSnewby <stephen.j.newby@gmail.com>
2025-07-31 18:39:41 -0500

    fixed last node handling, heap->size 0 handling, and betty

--- huffman_coding/heap/heap_extract.c
+++ huffman_coding/heap/heap_extract.c
@@ -15,59 +15,30 @@ static void swap_data(binary_tree_node_t *parent, binary_tree_node_t *new_node)
 }
 
 /**
- * heap_extract - extracts the root data
+ * heapify_down - moves a value from the top downwards in a min heap
  * @heap: heap
- *
- * Return: data or NULL on failure
  */
-void *heap_extract(heap_t *heap)
+void heapify_down(heap_t *heap)
 {
-	void *data = NULL;
-	int bitwise_pos = 0;
 	binary_tree_node_t *temp = NULL;
 
-	if (!heap || !heap->root || heap->size == 0)
-		return (NULL);
+	if (!heap)
+		return;
 
-	data = heap->root->data;
 	temp = heap->root;
 
-	while ((1U << (bitwise_pos + 1)) <= heap->size)
-		bitwise_pos++;
-
-	bitwise_pos--;
-	while (bitwise_pos > 0)
-	{
-		if (heap->size & (1U << (bitwise_pos + 1)))
-			temp = temp->right;
-		else
-			temp = temp->left;
-		bitwise_pos--;
-	}
-
-	heap->root->data = temp->data;
-
-	if (temp->parent && temp->parent->left && temp->parent->left == temp)
-		temp->parent->left = NULL;
-	else if (temp->parent && temp->parent->right)
-		temp->parent->right = NULL;
-	free(temp);
-	temp = NULL;
-
-	heap->size--;
-	if (heap->size == 0)
-		heap->root = NULL;
-	temp = heap->root;
 	while (temp)
 	{
 		if (temp->left && temp->right)
 		{
-			if (heap->data_cmp(temp->left->data, temp->right->data) < 0 && heap->data_cmp(temp->data, temp->left->data) > 0)
+			if (heap->data_cmp(temp->left->data, temp->right->data) < 0 &&
+			heap->data_cmp(temp->data, temp->left->data) > 0)
 			{
 				swap_data(temp, temp->left);
 				temp = temp->left;
 			}
-			else if (heap->data_cmp(temp->left->data, temp->right->data) > 0 && heap->data_cmp(temp->data, temp->right->data) > 0)
+			else if (heap->data_cmp(temp->left->data, temp->right->data) > 0 &&
+			heap->data_cmp(temp->data, temp->right->data) > 0)
 			{
 				swap_data(temp, temp->right);
 				temp = temp->right;
@@ -88,11 +59,81 @@ void *heap_extract(heap_t *heap)
 		else
 			break;
 	}
-	/** Once the data is extracted,
-	 * the root node must be freed, (i just swapped data)
-	 * and replaced by the last node of the heap.
-	 * Then the heap must be rebuilt if necessary
-	 */
+}
+
+/**
+ * last_node - finds last node in a heap
+ * @heap: heap
+ *
+ * Return: last node, or NULL if not found
+ */
+binary_tree_node_t *last_node(heap_t *heap)
+{
+	binary_tree_node_t *temp = NULL;
+	size_t path_mask = 1, i;
+
+	if (!heap || !heap->root || heap->size <= 0)
+		return (NULL);
+
+	if (heap->size == 1)
+		return (heap->root);
+
+	/* finds level of path to last node after root */
+	while ((path_mask << 1) <= heap->size)
+		path_mask <<= 1;
+
+	/* drive to last node */
+	temp = heap->root;
+	for (i = path_mask >> 1; i > 0; i >>= 1)
+	{
+		if (heap->size & i)
+			temp = temp->right;
+		else
+			temp = temp->left;
+	}
+	return (temp);
+}
+
+/**
+ * heap_extract - extracts the root data
+ * @heap: heap
+ *
+ * Return: data or NULL on failure
+ */
+void *heap_extract(heap_t *heap)
+{
+	void *data = NULL;
+	binary_tree_node_t *last = NULL;
+
+	if (!heap || !heap->root || heap->size == 0)
+		return (NULL);
+
+	data = heap->root->data;
+
+	last = last_node(heap);
+	if (!last)
+		return (NULL);
+	if (last == heap->root)
+	{
+		free(heap->root);
+		heap->root = NULL;
+		heap->size--;
+		return (data);
+	}
+
+	heap->root->data = last->data;
+	/* parent cleanup */
+	if (last->parent && last->parent->left && last->parent->left == last)
+		last->parent->left = NULL;
+	else if (last->parent && last->parent->right && last->parent->right == last)
+		last->parent->right = NULL;
+
+	/* free and downsize */
+	free(last);
+	last = NULL;
+	heap->size--;
+
+	heapify_down(heap);
 
 	return (data);
 }
