6d5b3163ce6817e2eebb1d99d11bf85aec7af7f7
TheSnewby <stephen.j.newby@gmail.com>
2025-08-01 08:53:28 -0500

    heap_delete

--- huffman_coding/heap/heap_delete.c
+++ huffman_coding/heap/heap_delete.c
@@ -1,4 +1,36 @@
 #include "heap.h"
 
-void heap_delete(heap_t *heap, void (*free_data)(void *));
+/**
+ * heap_delete_rec - recursive helper for heap_delete
+ * @root: root node
+ * @heap: heap
+ */
+void heap_delete_rec(binary_tree_node_t *root, void (*free_data)(void *))
+{
+	if (!root)
+		return;
 
+	if (root->left)
+		heap_delete_rec(root->left, free_data);
+	if (root->right)
+		heap_delete_rec(root->right, free_data);
+
+	free_data(root->data);
+	free(root);
+	root = NULL;
+}
+
+/**
+ * heap_delete - deallocates a heap
+ * @heap: heap to be deallocated
+ * @free_data: function to handle data
+ */
+void heap_delete(heap_t *heap, void (*free_data)(void *))
+{
+	if (!heap || !free_data)
+		return;
+
+	heap_delete_rec(heap->root, free_data);
+	free(heap);
+	heap = NULL;
+}
