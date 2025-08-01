f60d1a9e3b1f428a814b5dd2414b6f7c23a14139
TheSnewby <stephen.j.newby@gmail.com>
2025-08-01 09:26:50 -0500

    symbol.c

--- huffman_coding/symbol.c
+++ huffman_coding/symbol.c
@@ -1,1 +1,22 @@
-symbol_t *symbol_create(char data, size_t freq);
+#include "huffman.h"
+
+/**
+ * symbol_create - creates a symbol data structure
+ * @data: data to be stored
+ * @freq: associated frequency
+ *
+ * Return: new symbol or NULL on failure
+ */
+symbol_t *symbol_create(char data, size_t freq)
+{
+	symbol_t *new = NULL;
+
+	new = (symbol_t *)malloc(sizeof(symbol_t));
+	if (!new)
+		return (NULL);
+
+	new->data = data;
+	new->freq = freq;
+
+	return (new);
+}
