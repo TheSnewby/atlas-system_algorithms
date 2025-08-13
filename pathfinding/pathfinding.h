5f87d2b72999d8917ce40f3264c7044db0b5e28f
TheSnewby <stephen.j.newby@gmail.com>
2025-08-13 16:48:50 -0500

    betty for header

--- pathfinding/pathfinding.h
+++ pathfinding/pathfinding.h
@@ -7,8 +7,10 @@
 #include "graphs.h"
 #include "queues.h"
 
-#define POINT_CMP(a, b) (!(a) || !(b) ? -1 : ((a)->x == (b)->x && (a)->y == (b)->y) ? 1 : 0)
-#define VERTEX_CMP(a, b) (!(a) || !(b) ? -1 : (strcmp(a->content, b->content) == 0) ? 1 : 0)
+#define POINT_CMP(a, b) (!(a) || !(b) ? -1 : \
+((a)->x == (b)->x && (a)->y == (b)->y) ? 1 : 0)
+#define VERTEX_CMP(a, b) (!(a) || !(b) ? -1 : \
+(strcmp(a->content, b->content) == 0) ? 1 : 0)
 
 /**
  * struct point_s - Structure storing coordinates
@@ -18,12 +20,15 @@
  */
 typedef struct point_s
 {
-    int x;
-    int y;
+	int x;
+	int y;
 } point_t;
 
-queue_t *backtracking_array(char **map, int rows, int cols, point_t const *start, point_t const *target);
-queue_t *backtracking_graph(graph_t *graph, vertex_t const *start, vertex_t const *target);
-queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start, vertex_t const *target);
+queue_t *backtracking_array(char **map, int rows, int cols,
+	point_t const *start, point_t const *target);
+queue_t *backtracking_graph(graph_t *graph, vertex_t const *start,
+	vertex_t const *target);
+queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
+	vertex_t const *target);
 
 #endif
