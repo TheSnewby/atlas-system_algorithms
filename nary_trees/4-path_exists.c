c0494d6ef9add4b818eb7544ddcbf92cf7261cbd
TheSnewby <stephen.j.newby@gmail.com>
2025-08-20 00:55:41 -0500

    4-path_exists.c

--- nary_trees/4-path_exists.c
+++ nary_trees/4-path_exists.c
@@ -12,5 +12,44 @@
  */
 int path_exists(nary_tree_t const *root, char const * const *path)
 {
-	
+	int i = 0, not_found = 0;
+	nary_tree_t const *temp = NULL;
+
+	if (!root || !path)
+		return (0);
+	temp = root;
+	if (strcmp(temp->content, path[i]) == 0)
+	{
+		i++;
+		temp = temp->children;
+	}
+	else
+		return (0);
+	while (temp && path[i] && !not_found)
+	{
+		if (strcmp(temp->content, path[i]) == 0) /* check child */
+		{
+			i++;
+			temp = temp->children;
+		}
+		else /* check siblings */
+		{
+			temp = temp->next;
+			while (temp)
+			{
+				if (strcmp(temp->content, path[i]) == 0)
+				{
+					i++;
+					temp = temp->children;
+					break;
+				}
+				temp = temp->next;
+			}
+		}
+		if (!temp && path[i])
+			not_found++;
+	}
+	if (not_found)
+		return (0);
+	return (1);
 }
